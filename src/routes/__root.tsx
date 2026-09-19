10da2ffc97506a788f1cd07bf961362ec5caab16
uverley13 <jhojana746@gmail.com>
2026-09-19 09:18:05 -0500

    Create the root route layout required by TanStack Router to fix the broken app shell.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,1 +1,15 @@
-### 
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+
+import '../styles.css'
+
+export const Route = createRootRoute({
+  component: RootLayout,
+})
+
+function RootLayout() {
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
